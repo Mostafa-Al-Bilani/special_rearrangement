5abc03705277d7dce0649e8336f23670569453fb
Mostafa-Al-Bilani <mostafaalbilani@gmail.com>
2024-11-14 14:02:56 +0200

    cleaned the code

--- special_rearrangement.py
+++ special_rearrangement.py
@@ -3,13 +3,15 @@ nums = [1,2,4,5,7,9,6]
 LENGTH = len(nums)
 def special_rearrangement(nums):
     new_list = []
-    odd_list = []  
+    odd_list = []
     for i in range(LENGTH):
+
         if nums[i] % 2 == 0:
             new_list.append(nums[i])
-        if nums[i] % 2 != 0: 
+
+        if nums[i] % 2 != 0:
             odd_list.append(nums[i])
-    new_list.extend(odd_list) 
+
+    new_list.extend(odd_list)
     return new_list
 print(special_rearrangement(nums))
-
